940df1ebd108b0ff3168abd5c0e623d95a022e02
SoonHee <Vin.cent@binus.ac.id>
2022-06-08 16:55:18 +0700

    Update
    Update for trying using Dynamic Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import logo from "./logo.svg";
 import Card from "./Component/Card";
 import "./App.css";
+import Tag from "./Component/Tag";
 
 function App() {
+
+
+  // const tag = ['#value1','#value2','#value3']
+
+  const image= require(".//Images/Sunset.png");
+
+  const CardShow = () => {
+      for(let i=0 ;i<dataSet.length;i++){
+
+      }
+  }
+
+  const dataSet = [
+    {
+      image: require(".//Images/Sunset.png"),
+      Hashtag : ['#value1 ',' #value2 '],
+    },
+  ]
+
   return (
     <div className="App">
-      <div className="flex flex-row justify-around">
-        <Card />
-        <Card />
+      <div className="flex flex-row justify-evenly">
+        <Card title="Testing" desc="Testing dlu ya bisa apa engga" tag="#JS" img={image} />
+        <Card title="Testing" desc="Testing dlu ya bisa apa engga" tag=
+        {dataSet.map((data) => (
+          data.Hashtag.map((Hashtag) => (
+              <Tag tag={Hashtag}/>
+              ))  
+          ))
+        }
+        img={dataSet[0].image}/>
         <Card />
       </div>
     </div>
