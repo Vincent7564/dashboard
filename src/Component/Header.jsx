e3c67aa118beaf4492e9d5e82ca08552fa6bc301
SoonHee <Vin.cent@binus.ac.id>
2023-04-17 15:18:02 +0700

    Commit
    Add about me and hyperlink

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -71,9 +71,6 @@ export default function Header() {
                       </Link>
                     ))}
           </div>
-          <div className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
-              <Link to={"./SignUp"}>Login</Link>
-          </div>
         </div>
       </nav>
 
