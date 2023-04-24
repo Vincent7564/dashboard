492122365b34fde13070e36ddc975295e1f90c8a
SoonHee <Vin.cent@binus.ac.id>
2023-04-24 17:42:15 +0700

    Revamp Calendar
    Revamp github Calendar into this project

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -1,17 +1,22 @@
 import GitHubCalendar from "react-github-calendar";
-
+import Github from '../Images/github-mark.svg';
 const Calendar = () => {
+
   return (
-    <div>
-        <span className="flex pt-10 align-middle text-2xl">
-        Github Activity
+    <div className="bg-black">
+        <span className="flex pt-10 align-middle text-2xl text-white">
+        <a href="https://github.com/Vincent7564">
+        <img src={Github} alt="git" width="50px" height= "50px" /></a> &nbsp; Github Activity
         </span>
-      <div className="pl-10 pt-10 flex align-middle">
+      <div className="pl-10 pt-10 pb-10 flex align-middle text-white">
         <GitHubCalendar
           username="vincent7564"
           blockMargin={10}
           blockRadius={2}
           blockSize={16}
+          showWeekdayLabels={true}
+          colorScheme="light"
+          
         />
       </div>
     </div>
