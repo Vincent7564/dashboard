c0cca4e40e32e9b99d9881be20fde666be9a2929
SoonHee <Vin.cent@binus.ac.id>
2023-04-24 14:37:00 +0700

    Revert "Commit"
    This reverts commit e3c67aa118beaf4492e9d5e82ca08552fa6bc301.

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,45 +1,26 @@
-import "./AboutMe.css";
-
 const AboutMe = () => {
-  return (
-    <div className="text-center text-xl pt-5">
-      You can Reach me At
-      <div className="flex justify-center pr-20">
-        <div className="">
-          <a href="https://mail.google.com/mail/">
-            <div className="flex justify-between w-[50px] h-[50px] pt-5">
-              <img src={require("../Images/gmail.png")} alt="" />
-              <div className="align-middle pl-3 text-sm">Vincent7564756@gmail.com</div>
+    return (
+        <div>
+            <div className="">
+                Reach Me At
             </div>
-          </a>
-          <a href="https://www.instagram.com/vincent756">
-            <div className="flex justify-between w-[50px] h-[50px] pt-5">
-              <img src={require("../Images/instagram.png")} alt="" />
-              <div className="align-middle pl-3 text-sm">@vincent756</div>
+            
+            <div>
+                <div>
+                    <img src="https://www.flaticon.com/free-icons/gmail" alt="" />Gmail
+                </div>
+                <div>
+                    <img src="https://www.flaticon.com/free-icons/instagram" alt=""/>Instagram
+                </div>
+                <div>
+                    <img src="https://www.flaticon.com/free-icons/linkedin" alt=""/>Linked In
+                </div>
+                <div>
+                    <img src="https://www.flaticon.com/free-icons/discord" alt=""/>Soon Hee#1187
+                </div>
             </div>
-          </a>
-          <a href="https://www.linkedin.com/in/vin-cent/">
-            <div className="flex w-[50px] h-[50px] pt-5">
-              <img src={require("../Images/linkedin.png")} alt="" />
-              <span className="align-middle pl-3 text-sm">
-                <span className="pr-1">Vin </span>
-                <span> Cent</span>
-              </span>
-            </div>
-          </a>
-          <a href="https://mail.google.com/mail/">
-            <div className="flex w-[50px] h-[50px] pt-5">
-              <img src={require("../Images/discord.png")} alt="" />
-              <span className="align-middle pl-3 text-sm">
-                <span className="pr-1">Soon </span>
-                <span> Hee#1187</span>
-              </span>
-            </div>
-          </a>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AboutMe;
