6c8734c336684c7f6b0eae8686c56859ee5424d7
SoonHee <Vin.cent@binus.ac.id>
2023-06-25 12:19:05 +0700

    Modification Structure

--- src/Routes/RoutesList.jsx
+++ src/Routes/RoutesList.jsx
@@ -1,12 +1,26 @@
-import { Router,Routes,Route,BrowserRouter } from "react-router-dom";
+import { Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import AboutMe from "../Pages/AboutMe";
+import Calendar from "../Pages/Calendar";
+import Language from "../Pages/Language";
+import Project from "../Pages/Project";
+import Dashboard from "../Component/Dashboard";
+import WorkingExperience from "../Pages/WorkingExperience";
+import Certification from "../Pages/Certification";
 
-const RouteList = () =>{
-        <BrowserRouter>
-            <Router>
-                <Routes path='/about-me' element={<AboutMe />} />
-            </Router>
-        </BrowserRouter>
+const RouteList = () => {
+    <>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/about-me" element={<AboutMe />} />
+      <Route path="/lang" element={<Language />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/experience" element={<WorkingExperience />} />
+      <Route path="/certification" element={<Certification />} />
+    </Routes>
+  </BrowserRouter>;
+  </>
 };
 
-export default RouteList();
+export default RouteList;
--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import RoutesList from './Routes/RoutesList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+  
   <React.StrictMode>
     <Header />
+    {/* <RoutesList /> */}
   </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
